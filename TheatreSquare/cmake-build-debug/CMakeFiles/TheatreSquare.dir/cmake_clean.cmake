5b55539b3224150aa82d8021629a75611d149cc3
abdullahazab <abdullah.a.azab@gmail.com>
2021-09-06 14:29:57 +0200

    new commit

---
+++ TheatreSquare/cmake-build-debug/CMakeFiles/TheatreSquare.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TheatreSquare.dir/main.cpp.obj"
+  "TheatreSquare.exe"
+  "TheatreSquare.exe.manifest"
+  "TheatreSquare.pdb"
+  "libTheatreSquare.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TheatreSquare.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
